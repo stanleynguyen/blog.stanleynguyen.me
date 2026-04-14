8b0d96a766363cc3410bd741221e7c15ab56f142
stanleynguyen <stanleynguyen@users.noreply.github.com>
2026-04-14 03:46:55 +0000

    deploy: 98f5c38ff16df2ba656e62aa988acefa5421800f

--- js/theme-mode.js
+++ js/theme-mode.js
@@ -1,11 +1,12 @@
 function switchTheme() {
-  const currentStyle = currentTheme();
-
-  if (currentStyle == 'light') {
-    setTheme('dark');
+  const currentStyle = currentTheme()
+  if (currentStyle === 'light') {
+    setTheme('dark')
+    setIconTheme('dark')
   }
   else {
-    setTheme('light');
+    setTheme('light')
+    setIconTheme('light')
   }
 }
 
@@ -15,6 +16,28 @@ function setTheme(style) {
   });
   document.documentElement.setAttribute('data-color-mode', style);
   localStorage.setItem('data-color-mode', style);
+}
+
+function setIconTheme(theme) {
+  const twitterIconElement = document.getElementById('twitter-icon')
+  const githubIconElement = document.getElementById('github-icon')
+  if (twitterIconElement) {
+    if (theme === 'light') {
+      twitterIconElement.setAttribute("fill", "black")
+    } else if (theme === 'dark') {
+      twitterIconElement.setAttribute("fill", "white")
+    }
+  }
+
+  if (githubIconElement) {
+    if (theme === 'light') {
+      githubIconElement.removeAttribute('color')
+      githubIconElement.removeAttribute('class')
+    } else if (theme === 'dark') {
+      githubIconElement.setAttribute('class', 'octicon')
+      githubIconElement.setAttribute('color', '#f0f6fc')
+    }
+  }
 }
 
 function currentTheme() {
